bc6c06e4851aa7001dd78100e909366c8eef8e7f
Mark Karpeles <magicaltux@gmail.com>
2022-02-17 00:44:21 +0900

    add new file

---
+++ interco.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"io"
+	"net"
+	"sync"
+
+	"github.com/golang/snappy"
+)
+
+// interconnect c1 to c2 with optional snappy compression/decompression
+// both c1 and c2 will be closed on completion
+func interco(c1, c2 net.Conn, compress bool) {
+	cl := make(chan struct{})
+	once := sync.Once{}
+	doClose := func() {
+		once.Do(func() {
+			close(cl)
+		})
+	}
+
+	// closing both connections will trigger read failure and close the following goroutines
+	defer c1.Close()
+	defer c2.Close()
+
+	// c1→c2
+	go func() {
+		defer doClose()
+		if compress {
+			// snappy compression
+			io.Copy(snappy.NewWriter(c1), c2)
+		} else {
+			io.Copy(c1, c2)
+		}
+	}()
+
+	// c2→c1
+	go func() {
+		defer doClose()
+		if compress {
+			// snappy decompression
+			io.Copy(c2, snappy.NewReader(c1))
+		} else {
+			io.Copy(c2, c1)
+		}
+	}()
+
+	// wait for close signal
+	select {
+	case <-cl:
+	}
+}
